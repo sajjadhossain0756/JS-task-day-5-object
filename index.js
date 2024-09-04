39c2bfad5026403889ca3a07a4880d2c74488296
sajjadhossain <sajjadhossain0756@gmail.com>
2024-09-04 21:31:46 +0600

    task-1 complete

---
+++ index.js
@@ -0,0 +1,12 @@
+// Js object task start here;
+// task-1 start here;
+// Access the golden rod color value in output
+
+const colors = {
+    red: "#ff0000",
+    green: "#00ff00",
+    blue: "#0000ff",
+    "golden rod": '#daa520'
+};
+
+console.log(colors["golden rod"]);
