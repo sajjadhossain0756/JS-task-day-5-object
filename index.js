318a20c65a8e0b778dbb389a5ca9661dc3228ea9
sajjadhossain <sajjadhossain0756@gmail.com>
2024-09-04 21:44:02 +0600

    task-2 complete

--- index.js
+++ index.js
@@ -10,3 +10,16 @@ const colors = {
 };
 
 console.log(colors["golden rod"]);
+
+// task-2 start here
+// For this object below add a property named passenger capacity with value 5;
+
+const car = {
+    make: "Toyota",
+    model: "Corolla",
+    year: 2020
+};
+
+car["passenger capacity"] = 5 ;
+
+console.log(car);
